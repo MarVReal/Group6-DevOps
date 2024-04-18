cfe5cee6f4d17443b8a47173b50f0680b0684d08
JJLQuintana <qjjlquintana@tip.edu.ph>
2024-04-18 11:06:07 +0000

    added local ip of the device

--- trial.py
+++ trial.py
@@ -1,4 +1,5 @@
 import requests
+import socket
 
 def get_ip():
     response = requests.get('https://api.ipify.org?format=json').json()
@@ -8,7 +9,7 @@ def get_location():
     ip_address = get_ip()
     response=requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     location_data = {
-        "IPv4 Address": ip_address, 
+        "Public IPv4 Address": ip_address, 
         "City": response.get("city"),
         "Region": response.get("region"),
         "Country": response.get("country_name"),
@@ -22,7 +23,14 @@ def get_location():
     }
     return location_data
 
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+
+
 location_data = get_location()
-print("The user's IPv4 address and current location is as follows:")
+print("The user's Public and Local IPv4 address and current location is as follows:")
 for key, value in location_data.items():
     print(f"{key}: {value}")
